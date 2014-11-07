0c5c072fb85b4a8bebbe260bf5942623016c1e4b
roger.chen <roger.chen@dianping.com>
2014-11-07 11:56:32 +0800

    d34

---
+++ openrice/down4.rb
@@ -0,0 +1,18 @@
+require_relative 'request'
+
+Openrice.new(nil).init_folder
+
+logger = Logger.new('system.log')
+
+threads = []
+
+(131..200).map do |r|
+  (0..9).map do |t|
+    threads << Thread.new { ((t * 100 + 1000 * r )..(t + 1) * 100 + 1000 * r - 1).each{|num| Openrice.new(num).download } }
+  end
+
+  logger.info "#{r} #{threads}"
+  threads.each { |thread| thread.join }
+  logger.info "#{r} #{threads} ends"
+  threads = []
+end  
