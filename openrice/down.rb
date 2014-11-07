f607b1ef0e98fe2c2045fe6530b14a9911235de2
roger.chen <roger.chen@dianping.com>
2014-11-07 11:14:47 +0800

    add thread

--- openrice/down.rb
+++ openrice/down.rb
@@ -7,8 +7,8 @@ logger = Logger.new('system.log')
 threads = []
 
 (0..40).map do |r|
-  (0..4).map do |t|
-    threads << Thread.new { ((t * 200 + 1000 * r )..(t + 1) * 200 + 1000 * r - 1).each{|num| Openrice.new(num).download } }
+  (0..9).map do |t|
+    threads << Thread.new { ((t * 100 + 1000 * r )..(t + 1) * 100 + 1000 * r - 1).each{|num| Openrice.new(num).download } }
   end
 
   logger.info "#{r} #{threads}"
